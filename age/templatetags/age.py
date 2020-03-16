afcde77062dcbe57709d5e8b500a1933d2567609
peachparfait <peachparfait10@gmail.com>
2020-03-16 11:53:18 +0900

    年齢表示を完璧に

--- age/templatetags/age.py
+++ age/templatetags/age.py
@@ -11,4 +11,16 @@ def showage(value, args):
     now = datetime.datetime.now()
     timezone = pytz.timezone('Asia/Tokyo')
     now = timezone.localize(now)
-    return now.year - args.year
+    if now.month > args.month: #その一年で誕生日の月が過ぎている
+        return now.year - args.year
+    elif now.month == args.month: #誕生月
+        if now.day == args.day: #今日が誕生日！！！！！！
+            return now.year - args.year
+        elif now.day > args.day: #誕生日は過ぎている
+            return now.year - args.year
+        else: #まだ誕生日を迎えていない
+            return now.year - args.year - 1
+    elif now.year - args.year <= 0: #マイナス防止
+        return 0
+    else:
+        return now.year - args.year - 1 #まだ誕生日を迎えていない
